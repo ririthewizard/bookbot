624031234882d32cfa21a88ab11e5c35ce7b57e4
Riley Williams <ririwilliams.rw@gmail.com>
2024-10-29 16:46:00 -0500

    Transforming dict to list of dicts (attempting to anyways) and printing it to the console in a pretty fashion

--- main.py
+++ main.py
@@ -1,17 +1,41 @@
 def main():
-    with open("books/frankenstein.txt") as f:
-        file_contents = f.read()
-        #print(file_contents)
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    num_words = get_num_words(text)
+    chars_dict = get_character_appearances(text)
+    print_report(chars_dict)
+    print(f"--- Begin report of {book_path} ---")
+    print(f"{num_words} words found in the document \n")
+    #print(chars_dict)
 
-    def wordsInBook():
-        book_string = file_contents.split()
-        print(len(book_string))
-        return len(book_string)
+def get_num_words(text):
+    words = text.split()
+    return len(words)
     
-    def characterAppearances():
-        text = wordsInBook()
-        
+def get_character_appearances(text):
+    character_dict = {}
+    for char in text:
+        c = char.lower()
+        if c not in character_dict:
+            character_dict[c] = 1
+        else:
+            character_dict[c] += 1
+    #print(character_dict)
+    return character_dict
     
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
+def print_report(dict):
+    tmp = []
+    for key, value in dict.items():
+        if key.isalpha():
+            tmp.append({key, value})
+    for d in tmp:
+        d.type()
+        print(f"The /'{d[0]}' was found /'{d[1]}' times")
+
 
 
 main()
