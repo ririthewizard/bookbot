20f0defa0fc30d66bcf3003571d50b652656daed
Riley Williams <ririwilliams.rw@gmail.com>
2024-10-29 17:07:02 -0500

    List comprehensions save the day! Also made sure to use the key and value names to access them inside the list itself

--- main.py
+++ main.py
@@ -3,10 +3,13 @@ def main():
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     chars_dict = get_character_appearances(text)
-    print_report(chars_dict)
+    sorted = print_report(chars_dict)
     print(f"--- Begin report of {book_path} ---")
     print(f"{num_words} words found in the document \n")
     #print(chars_dict)
+    for char in sorted:
+        print(f"The '{char["character"]}' character was found {char["num"]} times")
+
 
 def get_num_words(text):
     words = text.split()
@@ -16,9 +19,9 @@ def get_character_appearances(text):
     character_dict = {}
     for char in text:
         c = char.lower()
-        if c not in character_dict:
+        if c not in character_dict and c.isalpha():
             character_dict[c] = 1
-        else:
+        elif c.isalpha():
             character_dict[c] += 1
     #print(character_dict)
     return character_dict
@@ -27,14 +30,16 @@ def get_book_text(path):
     with open(path) as f:
         return f.read()
 
+def sort_on(dict):
+    return dict["num"]
+
 def print_report(dict):
     tmp = []
-    for key, value in dict.items():
-        if key.isalpha():
-            tmp.append({key, value})
-    for d in tmp:
-        d.type()
-        print(f"The /'{d[0]}' was found /'{d[1]}' times")
+    for key in dict.keys():
+        tmp = [{"character": k, "num": v} for k,v in dict.items()]
+    tmp.sort(reverse=True, key=sort_on)
+    print(type(tmp))
+    return tmp
 
 
 
